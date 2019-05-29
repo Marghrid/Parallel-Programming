04335c0d63a5d846cfbc23574ca42915d4b40e58
Marghrid <marghrid@hotmail.com>
2019-05-29 20:56:47 +0200

    assignment 4 2

--- assignment4/student/familytree_par.c
+++ assignment4/student/familytree_par.c
@@ -1,25 +1,31 @@
 #include "familytree.h"
 
+int traverse_rec(tree *node) {
+	if (node == NULL) return 0;
+
+	int father_iq, mother_iq;
+
+	#pragma omp task
+	father_iq = traverse_rec(node->father);
+
+	#pragma omp task
+	mother_iq = traverse_rec(node->mother);
+
+	#pragma omp task
+	{
+		node->IQ = compute_IQ(node->data, father_iq, mother_iq);
+		genius[node->id] = node->IQ;
+	}
+	
+    return node->IQ;
+}
+
 int traverse(tree *node, int numThreads) {
 	
 	if (node == NULL) return 0;
 
-    #pragma omp parallel num_threads(numThreads)
-    {
-		#pragma omp single
-		{
-			int father_iq, mother_iq;
+    #pragma omp parallel single nowait firstprivate(node) num_threads(numThreads)
+	traverse_rec(node);
 
-    		#pragma omp task
-			father_iq = traverse(node->father, numThreads);
-
-			#pragma omp task
-			mother_iq = traverse(node->mother, numThreads);
-
-			#pragma omp task
-			node->IQ = compute_IQ(node->data, father_iq, mother_iq);
-		}
-	}
-	genius[node->id] = node->IQ;
 	return node->IQ;
 }
