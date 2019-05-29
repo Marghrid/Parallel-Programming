fe67cb31af5de96cb0e2a6bf2ae0a389227856e6
Marghrid <marghrid@hotmail.com>
2019-05-29 18:29:32 +0200

    assignment4 1

--- assignment4/student/familytree_par.c
+++ assignment4/student/familytree_par.c
@@ -1,8 +1,25 @@
 #include "familytree.h"
 
-int traverse(tree *node, int numThreads){
+int traverse(tree *node, int numThreads) {
 	
-	// TODO implement your solution in here.
+	if (node == NULL) return 0;
 
-    return node->IQ;
+    #pragma omp parallel num_threads(numThreads)
+    {
+		#pragma omp single
+		{
+			int father_iq, mother_iq;
+
+    		#pragma omp task
+			father_iq = traverse(node->father, numThreads);
+
+			#pragma omp task
+			mother_iq = traverse(node->mother, numThreads);
+
+			#pragma omp task
+			node->IQ = compute_IQ(node->data, father_iq, mother_iq);
+		}
+	}
+	genius[node->id] = node->IQ;
+	return node->IQ;
 }
