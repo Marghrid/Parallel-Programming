34c5ba914856c3b012e727fc7b650ff684a7ec1b
Margarida Ferreira <ge52zal@mytum.de>
2019-06-21 10:51:45 +0200

    assignment6 done.

--- assignment6/student/loop_fusion_par.c
+++ assignment6/student/loop_fusion_par.c
@@ -1,32 +1,24 @@
 
 void compute(double **a, double **b, double **c, double **d, int N, int num_threads) {
 
-  /*TODO:
-    Apply loop optimisations to the code below. Think about when certain optimisations are applicable and why.
-    For example, when should you apply loop fusion? Should you do this for large problem sizes or small or both?
-    Alternatively, does it make sense to break apart a heavy loop with a lot of computations? Why?
-   
+/*
+    Apply loop optimisations to the code below. Think about when certain optimisations
+      are applicable and why.
+    For example, when should you apply loop fusion? Should you do this for large problem
+      sizes or small or both?
+    Alternatively, does it make sense to break apart a heavy loop with a lot of
+      computations? Why?
+  */
 
 
+  #pragma omp parallel for num_threads(num_threads)
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  	a[i][0] = 2 * c[i][0];
+    d[i][0] = a[i][0] * b[i][0];
+    for (int j = 1; j < N; j++) {
       a[i][j] = 2 * c[i][j];
+      d[i][j] = a[i][j] * b[i][j];
+      d[i][j-1] = d[i][j] + c[i][j-1];
     }
   }
-
-  for (int i = 0; i < N; i++) {
-    for (int j = 0 ; j < N; j++) {
-      d[i][j] = a[i][j] * b[i][j] ;
-    }
-  }
-
-
-  for (int j = 0; j < N-1; j++) {
-    for (int i = 0; i < N; i++) {
-      d[i][j] = d[i][j+1] + c[i][j];
-    }
-  }
-*/
-
-
 }
